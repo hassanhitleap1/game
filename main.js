c150609926c78cf603e3087e6150251c884960ba
hasan <hassanhitleap1@gmail.com>
2018-10-30 11:37:18 +0300

    add js

---
+++ main.js
@@ -0,0 +1,40 @@
+class Shape {
+    
+    constructor(arr) {
+        this.points=arr;
+    }
+
+    // Adding a method to the constructor
+    isCircle() {
+       if(true){
+        console.log("isCircle");
+       }else{
+        console.log("not isCircle");
+       }
+    }
+
+    isRectangle(){
+        if(true){
+            console.log("isRectangle");
+           }else{
+            console.log("not isRectangle");
+           }
+    }
+
+    
+    isTriangle(){
+        if(true){
+            console.log("isTriangle");
+           }else{
+            console.log("not isTriangle");
+           }
+    }
+
+    isSquare() {
+        if(true){
+            console.log("isSquare");
+           }else{
+            console.log("not isSquare");
+           } 
+    }
+}
