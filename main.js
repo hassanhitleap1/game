94e1ff28a652fcb051c7ade8aaeca7a7362ca6a5
hasan <hassanhitleap1@gmail.com>
2018-10-31 16:10:32 +0300

    s

--- main.js
+++ main.js
@@ -2,14 +2,16 @@ class Shape {
     
     constructor(pointsArray) {
         this.points=pointsArray;
+        this.nodePoint=pointsArray.length;
+        console.log('initiozion object');
        
     }
 
     // Adding a method to the constructor
     isCircle() {
        
-        this.points.forEach(function(entry) {
-            console.log(entry);
+        this.points.forEach(function(arr) {
+            console.log(arr);
         });
        if(true){
         console.log("isCircle");
@@ -36,11 +38,27 @@ class Shape {
     }
 
     isSquare() {
-        if(true){
-            console.log("isSquare");
-           }else{
-            console.log("not isSquare");
-           } 
+        this.islineRow();
+    }
+
+    islineRow(){
+        points=this.points;
+        //console.log(this.points[0][0]);
+        //firstNodeLeft=(this.points[0][0]);
+        console.log(this.points);
+
+        for (let index = 0; index < this.points.length; index++) {
+           
+            console.log( this.points[index] );
+            
+        }
+   
+        // this.points.forEach(function(arr) {
+        //     console.log(arr);
+        //     // if(! firstNodeLeft==arr[0]){
+        //     //     console.log("not line");
+        //     // }
+        // });  
     }
 }
 
